fddcdc296788c3ebc600472a43bfe747beba5a3a
Burin Asavesna <burinasavesna@gmail.com>
2015-01-23 19:51:28 -0600

    show what i am saying

--- js/app.js
+++ js/app.js
@@ -4,6 +4,8 @@
   recognition.interimResults = true;
   recognition.onresult = function(event) {
     console.log(event);
+    var whatEl = document.getElementById('what');
+    whatEl.innerHTML = event.results[0][0].transcript;
   };
   recognition.start();
 })();
