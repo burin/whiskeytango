e66bf7da4a7b56d934e30d363ca1f7b8a9354cd8
Burin Asavesna <burinasavesna@gmail.com>
2015-01-23 20:01:10 -0600

    button to toggle recognition

--- js/app.js
+++ js/app.js
@@ -1,11 +1,32 @@
 (function(){
   var recognition = new webkitSpeechRecognition();
+  var recognizing = false;
+
   recognition.continuous = true;
   recognition.interimResults = true;
+  recognition.onstart = function() {
+    recognizing = true;
+  };
+
+  recognizing.onend = function() {
+    recognizing = false;
+  };
   recognition.onresult = function(event) {
     console.log(event);
     var whatEl = document.getElementById('what');
     whatEl.innerHTML = event.results[0][0].transcript;
   };
-  recognition.start();
+
+  var toggleRecognition = function() {
+    if (recognizing === false) {
+      recognition.start();
+    } else {
+      recognition.stop();
+    }
+  };
+
+  var toggleRecognitionButton = document.getElementById('toggle-recognition');
+
+  toggleRecognitionButton.addEventListener('click', toggleRecognition);
+
 })();
